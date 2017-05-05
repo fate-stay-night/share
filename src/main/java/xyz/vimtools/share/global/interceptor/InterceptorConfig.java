a954287ec3f202f33e71b7fcb57930becfe5c20e
zhangzheng <kezy-5566@163.com>
2017-05-05 18:50:08 +0800

    修改拦截器配置的拦截url

--- src/main/java/xyz/vimtools/share/global/interceptor/InterceptorConfig.java
+++ src/main/java/xyz/vimtools/share/global/interceptor/InterceptorConfig.java
@@ -50,6 +50,7 @@ public class InterceptorConfig extends WebMvcConfigurationSupport {
                 .addPathPatterns("/**")
                 .excludePathPatterns(
                         "/",
+                        "/user",
                         "/user/login"
                 );
     }
